f0532e6ea71fe39e6033577175b22562eb44dcfa
Gabriel Ulloa <gabriel.ulloa.e@gmail.com>
2024-11-28 20:28:10 -0300

    prueba de sacar foto

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from openai import OpenAI
+from photo import take_picture
+import base64
 
 client = OpenAI()
+
+take_picture()
+encoded_string = ""
+with open("temp.jpg", "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
 response = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[{"role": "system", "content": "You are a helpful assistant."}, {
-        "role": "user", "content": "Donde me recomiendas ir a comer?"}],
+        "role": "user", "content": [{"type": "text", "text": "Dime que hay en la foto"}, {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{encoded_string}"}}]
+    }],
     user="gsulloa@uc.cl",
     store=True,
     metadata={"user_id": "gsulloa@uc.cl", "type": "Demo"}
