4f094751fcb4fade2049d24bcaeb309251f5d7b4
Gabriel Ulloa <gabriel.ulloa.e@gmail.com>
2024-11-28 20:32:38 -0300

    ejemplo con audios

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from openai import OpenAI
 from photo import take_picture
+from audio import record_audio
 import base64
 
 client = OpenAI()
 
-take_picture()
-encoded_string = ""
-with open("temp.jpg", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+# take_picture()
+# encoded_string = ""
+# with open("temp.jpg", "rb") as image_file:
+#     encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[{"role": "system", "content": "You are a helpful assistant."}, {
-        "role": "user", "content": [{"type": "text", "text": "Dime que hay en la foto"}, {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{encoded_string}"}}]
-    }],
-    user="gsulloa@uc.cl",
-    store=True,
-    metadata={"user_id": "gsulloa@uc.cl", "type": "Demo"}
+# response = client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     messages=[{"role": "system", "content": "You are a helpful assistant."}, {
+#         "role": "user", "content": [{"type": "text", "text": "Dime que hay en la foto"}, {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{encoded_string}"}}]
+#     }],
+#     user="gsulloa@uc.cl",
+#     store=True,
+#     metadata={"user_id": "gsulloa@uc.cl", "type": "Demo"}
+# )
+
+record_audio()
+response = client.audio.transcriptions.create(
+    model="whisper-1",
+    file=open("temp.wav", "rb")
 )
 
 print(response)
